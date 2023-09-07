6a353e7df2e96c286038db39fb47d4d6d07cb8e5
Rafael Yasis <rafaelyasis@gmail.com>
2023-09-06 20:38:57 -0500

    finished portions of the assignment that we learned in class

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,50 @@
 import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
 function App() {
 
+  const [newAirline, setNewAirline] = useState('')
+
+  // Dispatch
+  const dispatch = useDispatch();
+
+  // target what is in the airline form
+  const airlineList = useSelector(store => store.airlineList);
+
+  // ON SUBMIT OF THE FORM
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Tell redux that we want to add the new Element
+    dispatch({
+      type:'ADD_AIRLINE',
+      //Pass in the element name, taht we're tracking in state
+      payload: newAirline 
+    })
+    // clear form field
+    setNewAirline('');
+  }
+
+
+  //RETURN
   return (
-    <div>
+    <div className="App">
+
       <h1>Redux Airport</h1>
-      <input placeholder='Airline Name' />
-      <button>Add Airline</button>
-      <table>{/* Airlines should be listed here */}</table>
+
+      <form onSubmit={handleSubmit}>
+      <input type="text" placeholder='Airline Name' value={newAirline} onChange={event => setNewAirline(event.target.value)} />
+      <button type='submit'>Add Airline</button>
+      </form>
+     
+      <table>
+        <tr>
+          <th>Airlines</th>
+        </tr>
+            {airlineList.map((airline, i) =>(
+              <tr key={i}>{airline}</tr>
+            ))}
+      </table>
+      
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,19 +3,42 @@ import ReactDOM from 'react-dom/client';
 import App from './components/App/App';
 
 /** TODO: import REDUX **/
-
+import {createStore, combineReducers, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import logger from 'redux-logger';
 
 /** TODO: Add REDUCERS */
-
+const airlineList = (state = [], action) => {
+    console.log('hi i\'m a reducer in airline list');
+    if (action.type === 'ADD_AIRLINE') {
+      console.log(`The airline added was ${action.payload}`);   
+      //[ old array contents, new item]
+      return [...state, action.payload]
+    }
+    return state
+  };
 
 /** TODO: Create store */
+const storeInstance = createStore(
 
+    combineReducers (
+    { 
+        airlineList
+    }
+    ),
+    applyMiddleware(
+      logger
+    )
+  );
 
 
 // Be sure to add the Provider! Just wrap App with it. Don't copy and paste from lecture, that will cause issues.
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <App />
+        <Provider store={storeInstance}>
+         <App />
+        </Provider>
+        
     </React.StrictMode>
 );
